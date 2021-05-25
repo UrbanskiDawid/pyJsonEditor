3f1a8de52ae2293c1a063b1125ffb05491d94182
Dawid Urbanski <git@dawidurbanski.pl>
2021-05-25 22:47:31 +0200

    lint parse.py

--- parse.py
+++ parse.py
@@ -1,94 +1,99 @@
+"""parse token list to object"""
 from typing import List
-
+import pytest
 class TokenList:
-    def __init__(self, l):
-        self.l = l.copy()
+    """helper class to iterate tokens"""
+    def __init__(self, tok):
+        self.tok = tok.copy()
         self.pos =0
 
     def pop(self):
+        """remove first token"""
         self.pos+=1
-        return self.l[self.pos-1]
+        return self.tok[self.pos-1]
 
-    def next(self):
-        return self.l[self.pos] if self.pos<len(self.l) else None
+    def peek(self):
+        """see first token"""
+        return self.tok[self.pos] if self.pos<len(self.tok) else None
 
-    def next_is(self, v):
-        return self.next()[0]== v if self.pos<len(self.l) else False
-        
-def eat_value(l: TokenList):
-    assert l.next_is('v')
-    ret = l.pop()
+    def next_is(self, tok_type):
+        """check type on firts token"""
+        return self.peek()[0]== tok_type if self.pos<len(self.tok) else False
+
+def eat_value(tok: TokenList):
+    """convert value tokens to object"""
+    assert tok.next_is('v')
+    ret = tok.pop()
     return ret[2]
-    
-def eat_object(l:TokenList):
-    ret={}
-    key=None
-    assert l.next_is('{'), 'not object'
-    l.pop()
-    while l.next():
-        if l.next_is('S'):
-            k,v=eat_string(l)
-            ret[k]=v
-        elif l.next_is(','):
-            l.pop()
-        elif l.next_is('}'):
-            return ret
-        else:
-            raise Exception('array error',l.next())
 
-def eat_array(l:TokenList):
-    ret=[]
-    assert l.next_is('['), 'not array'
-    l.pop()
-    while l.next():
-        val=None
-        if l.next_is('v'):
-            val=eat_value(l)
-        elif l.next_is('['):
-            val=eat_array(l)
-        elif l.next_is('{'):
-            val=eat_object(l)
-        elif l.next_is(','):
-            l.pop()
-            continue #skip iteration
-        elif l.next_is(']'):
-            l.pop()
+def eat_dict(tok:TokenList):
+    """convert dict tokens to object"""
+    ret={}
+    assert tok.next_is('{'), 'not object'
+    tok.pop()
+    while tok.peek():
+        if tok.next_is('S'):
+            key,val=eat_string(tok)
+            ret[key]=val
+        elif tok.next_is(','):
+            tok.pop()
+        elif tok.next_is('}'):
             return ret
         else:
-            raise Exception('array error',l.next())
+            raise Exception('array error',tok.peek())
+
+def eat_array(tok:TokenList):
+    """convert array tokens to object"""
+    ret=[]
+    assert tok.next_is('['), 'not array'
+    tok.pop()
+    while tok.peek():
+        val=None
+        if tok.next_is('v'):
+            val=eat_value(tok)
+        elif tok.next_is('['):
+            val=eat_array(tok)
+        elif tok.next_is('{'):
+            val=eat_dict(tok)
+        elif tok.next_is(','):
+            tok.pop()
+            continue #skip iteration
+        elif tok.next_is(']'):
+            tok.pop()
+            return ret
+        else:
+            raise Exception('array error',tok.peek())
         ret.append(val)
 
-def eat_string(l: TokenList):
-    assert l.next_is('S'), 'string is not string'
-    key=l.pop()[2]
-    assert l.next_is(':'), 'string is not string'
-    l.pop()
-    if l.next_is('v'):
-        val=eat_value(l)
-    elif l.next_is('['):
-        val=eat_array(l)
-    elif l.next_is('{'):
-        val=eat_object(l)
+def eat_string(tokens: TokenList):
+    """convert string tokens to object"""
+    assert tokens.next_is('S'), 'string is not string'
+    key=tokens.pop()[2]
+    assert tokens.next_is(':'), 'string is not string'
+    tokens.pop()
+    if tokens.next_is('v'):
+        val=eat_value(tokens)
+    elif tokens.next_is('['):
+        val=eat_array(tokens)
+    elif tokens.next_is('{'):
+        val=eat_dict(tokens)
     else:
-        raise Exception('string error',l.next())
+        raise Exception('string error',tokens.peek())
     return (key,val)
 
 
 def parse(tokens:List):
     """
-    Conver tokens into object (dict) 
+    Conver tokens into object (dict)
     """
-    l=TokenList(tokens)
-    return eat_object(l)
+    tok=TokenList(tokens)
+    return eat_dict(tok)
 
 
 
 
 
 #################################### TESTS ###################################
-
-import pytest
-
 testdata = [
 (
     [ ('{',0), ('}',1) ],
@@ -99,11 +104,18 @@ testdata = [
     {"a":'0'}
 ),
 (
-    [('{', 0), ('S', 1, 'a'), (':', 4), ('v', 5, '0'), (',', 6), ('S',7,'b'),(':',10), ('v',11,'1'),('}',12)],
+    [('{', 0),
+       ('S', 1, 'a'), (':', 4), ('v', 5, '0'),
+       (',', 6),
+       ('S',7,'b'),(':',10), ('v',11,'1'),
+      ('}',12)],
     {"a":'0',"b":'1'}
 ),
 (
-    [('{', 0), ('S', 1, 'a'), (':', 4), ('[', 5), ('v', 6, '1'), (',',7),('v',8,'2'), (']',9),('}',10)],
+    [('{', 0),
+     ('S', 1, 'a'), (':', 4),
+     ('[', 5), ('v', 6, '1'), (',',7),('v',8,'2'),(']',9),
+     ('}',10)],
     {"a":['1','2']}
 ),
 (
@@ -114,4 +126,5 @@ testdata = [
 
 @pytest.mark.parametrize("tokens,expected", testdata)
 def test_tokenize(tokens, expected):
+    """test tokenize method"""
     assert parse(tokens) == expected
