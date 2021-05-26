741b538c7c72fcfffab92feb3ade9a3d3ac26fcd
Dawid Urbanski <git@dawidurbanski.pl>
2021-05-27 01:22:10 +0200

    token_list.py

--- tree.py
+++ tree.py
@@ -1,8 +1,7 @@
 """parse token list to recursive JsonNodes"""
 from typing import List
 from tokenizer import tokenize,StringIO
-class TokenError(Exception):
-    """ parser exception"""
+from token_list import TokenList
 
 class JsonNode:
     """ json node with begin end end children"""
@@ -41,37 +40,6 @@ class JsonNode:
                 obj.type == self.type and\
                 obj.kids == self.kids and\
                 obj.name == self.name
-
-class TokenList:
-    """helper class to iterate tokens"""
-    def __init__(self, tok):
-        self.tok = tok.copy()
-        self.pos = 0
-
-    def raise_token_error(self,comment:str):
-        """ raport exceptions """
-        raise TokenError(f'TokenError at postion:{self.pos} {comment}')
-
-    def pop(self):
-        """remove first token"""
-        self.pos += 1
-        return self.tok[self.pos-1]
-
-    def peek(self):
-        """see first token"""
-        return self.tok[self.pos] if self.pos<len(self.tok) else None
-
-    def next_is(self, tok_type):
-        """check type on firts token"""
-        return self.peek()[0]== tok_type if self.pos<len(self.tok) else False
-
-    def expect(self, tok_type, comment=''):
-        """assert next token"""
-        next_tok = self.peek()
-        if not next_tok:
-            self.raise_token_error(f'expected: {tok_type} found: Nothing. {comment}')
-        if next_tok[0] != tok_type:
-            self.raise_token_error(f'expected: {tok_type} found: {next_tok[0]}. {comment}')
 
 
 def eat_value(tok: TokenList):
