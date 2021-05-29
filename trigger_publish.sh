37cd775fe5a5ecb28ef9d746ac20ebf7ea82b9f5
Dawid Urbanski <git@dawidurbanski.pl>
2021-05-29 05:46:07 +0200

    github new workflow

--- trigger_publish.sh
+++ trigger_publish.sh
@@ -3,5 +3,7 @@ current=`git tag -l | tail -n1`;
 new=`python3 -c "v='$current'.split('.'); v[2]=str(1+int(v[2])); print('.'.join(v))"`
 echo "$current -> $new"
 sed -i 's/^version.=.*/version = a/' pyproject.toml
+git add pyproject.toml
+git commit -m "net tag $new"
 git tag -a $new -m "new tag $new"
 git push --tag origin master
