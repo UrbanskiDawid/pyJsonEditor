059102689b39e85efdff36ee51fbdc13f2936219
Dawid Urbanski <git@dawidurbanski.pl>
2021-05-29 05:49:51 +0200

    fix publish trigger

--- trigger_publish.sh
+++ trigger_publish.sh
@@ -2,7 +2,7 @@
 current=`git tag -l | tail -n1`;
 new=`python3 -c "v='$current'.split('.'); v[2]=str(1+int(v[2])); print('.'.join(v))"`
 echo "$current -> $new"
-sed -i 's/^version.=.*/version = a/' pyproject.toml
+sed -i "s/^version.=.*/version = '$new'/" pyproject.toml
 git add pyproject.toml
 git commit -m "net tag $new"
 git tag -a $new -m "new tag $new"
