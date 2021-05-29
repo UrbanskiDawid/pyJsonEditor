57cffe478df7f60249ac3543820d096a673a5633
Dawid Urbanski <git@dawidurbanski.pl>
2021-05-29 08:06:24 +0200

    trigger publish fixes

--- trigger_publish.sh
+++ trigger_publish.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-current=`git tag -l | tail -n1`;
-new=`python3 -c "v='$current'.split('.'); v[2]=str(1+int(v[2])); print('.'.join(v))"`
+current=`grep version pyproject.toml | cut -d' ' -f3`
+
+echo $current
+python3 -c "v=$current.split('.'); print(v)"
+
+new=`python3 -c "v=$current.split('.'); v[2]=str(1+int(v[2])); print('.'.join(v))"`
 echo "$current -> $new"
 sed -i "s/^version.=.*/version = '$new'/" pyproject.toml
+git diff pyproject.toml
+#exit 1
 git add pyproject.toml
 git commit -m "net tag $new"
 git tag -a $new -m "new tag $new"
