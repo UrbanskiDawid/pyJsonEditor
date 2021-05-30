59b930359a5a92c3c926f714f65705cdb55c1932
Dawid Urbanski <git@dawidurbanski.pl>
2021-05-30 05:48:44 +0200

    fix __init__.py

--- pyjsonedit/__init__.py
+++ pyjsonedit/__init__.py
@@ -1,5 +1,5 @@
 """module exports"""
 from .tokenizer import tokenize
 from .main import string_to_tokens,string_to_tree,string_match_mark
-from .matcher import MatchException,match,print_matched
+from .matcher import MatchException,match,match_as_string
 from .tree import JsonNode, parse
