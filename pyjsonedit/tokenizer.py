f6c9ad577ce26885a68a30e817cd6794554ec4a9
Dawid Urbanski <git@dawidurbanski.pl>
2021-06-19 18:40:46 +0200

    update strings configuration

--- pyjsonedit/tokenizer.py
+++ pyjsonedit/tokenizer.py
@@ -1,36 +1,56 @@
 """tokenize readable handle to tokens"""
 
+def __eat_string(char, handle):
+    """
+      consume text starting and endig with 'char' 
+    """
+    start_c = char
+    mem = ""
+    while True:
+        c_prev = char
+        char = handle.read(1)
+        if not char:
+            break # fail: end of stream
+        if char == start_c and c_prev != "\\":
+            return (True,mem) # success
+        mem += char
+    return (False,mem) # unfinished error
+
 def tokenize(handle):
     """read handle and turn it into tokens"""
     run = True
     pos = -1
     mem = ""
+
     while run:
         pos += 1
         char = handle.read(1)
         if not char:
             run = False
             break
+
+        #normal mode
         if char in ['[', ']', '{', '}', ",", ":"]:
             if mem.strip():
                 yield("v", pos-len(mem), mem)
             mem = ""
-            yield (char, pos)  # open
+
+            yield (char, pos)
+
+        #string mode
         elif char in ['"', "'"]:
-            start_c = char
-            pos_start = pos
+            if mem.strip():
+                yield("v", pos-len(mem), mem)
             mem = ""
-            while True:
-                c_prev = char
-                char = handle.read(1)
-                pos += 1
-                if not char:
-                    run = False
-                    break
-                if char == start_c and c_prev != "\\":
-                    break
-                mem += char
-            yield ('S', pos_start, mem)
-            mem = ""
+
+            success, text = __eat_string(char, handle)
+            if success:
+                yield ('S', pos, text)
+            else:
+                yield ('v', pos, char+text) # failed string
+
+            pos += len(text)+1
+
+        # other chars
         else:
             mem += char
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -13,6 +13,22 @@ testdata = [
     [('{', 0), ('S', 1, 'a'), (':', 4), ('v', 5, '0'), ('}', 6)]
 ),
 (
+    '{"a":"0"}',
+    [('{', 0), ('S', 1, 'a'), (':', 4), ('S', 5, '0'), ('}', 8)]
+),
+(
+    '{"a": "0"}',
+    [('{', 0), ('S', 1, 'a'), (':', 4), ('S', 6, '0'), ('}', 9)]
+),
+(
+    '{"a": "1234"}',
+    [('{', 0), ('S', 1, 'a'), (':', 4), ('S', 6, '1234'), ('}', 12)]
+),
+(   #note: with error
+    '{"a":XX"0"}',
+    [('{', 0), ('S', 1, 'a'), (':', 4), ('v',5,'XX'), ('S', 7, '0'), ('}', 10)]
+),
+(
     '{"a":0,"b":1}',
     [('{', 0),
       ('S', 1, 'a'), (':', 4), ('v', 5, '0'),
@@ -33,9 +49,9 @@ testdata = [
     '{"a":{}}',
     [('{', 0), ('S', 1, 'a'), (':', 4), ('{', 5), ('}', 6), ('}', 7)]
 ),
-(
+(#handle error incomplete string
     '{"a',
-    [('{', 0), ("S",1,'a')]
+    [('{', 0), ("v",1,'"a')]
 ),
 (
     '{ "a": 123 }',
@@ -59,3 +75,25 @@ def test_tokenize(json, expected):
     handle = StringIO(json)
     ret=list(tokenize(handle))
     assert ret == expected
+
+
+@pytest.mark.parametrize("json,expected", testdata)
+def test_tokenize_positions_and_payloads_are_correct(json, expected):
+    """ test tokenize method"""
+    print(expected) #lint unused arg
+    handle = StringIO(json)
+    for token_id, token in enumerate(tokenize(handle)):
+        pos = token[1]
+        typ = token[0]
+        if typ in ['[', ']', '{', '}', ",", ":"]:
+            assert json[pos] == typ, f'{token_id} failed "{typ}" != "{json[pos]}" '
+        elif typ == 'v':
+            payload = token[2]
+            text = json[pos:pos+len(payload)]
+            assert text == payload
+        elif typ == 'S':
+            payload = token[2]
+            text = json[pos:pos+len(payload)+2]
+            assert text in [f'"{payload}"', f"'{payload}'"]
+        else:
+            raise ValueError(f'unknown token type: {typ}')
